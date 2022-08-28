09e6cae5ebbcf9f6e2905502891595a34659ba8d
Fede Rojo <federojo10@gmail.com>
2022-08-28 19:12:02 -0300

    GUI y JAR creados

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -9,6 +9,20 @@ import javax.swing.border.EmptyBorder;
 
 import entities.Student;
 import javax.swing.JTabbedPane;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,17 +31,23 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JTextField txtLU;
+	private JTextField txtApellido;
+	private JTextField txtNombre;
+	private JTextField txtGithub;
+	private JTextField txtEmail;
+	private JTextPane txtHora;
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentaciÃ³n");
+		setTitle("TdP-DCIC-UNS 2021 :: Pantalla de presentación");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(new Dimension(615, 250));
+		setSize(new Dimension(606, 276));
 		setResizable(false);
 		setContentPane(contentPane);
 		
@@ -39,7 +59,144 @@ public class SimplePresentationScreen extends JFrame {
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("InformaciÃ³n del alumno", null, tabInformation, "Muestra la informaciÃ³n declarada por el alumno");
-		contentPane.add(tabbedPane, BorderLayout.WEST);
+		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		
+		txtLU = new JTextField();
+		txtLU.setColumns(10);
+		txtLU.setText(""+studentData.getId());
+		
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		txtApellido.setText(studentData.getLastName());
+		
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setText(studentData.getFirstName());
+		
+		txtGithub = new JTextField();
+		txtGithub.setColumns(10);
+		txtGithub.setText(studentData.getGithubURL());
+		
+		txtEmail = new JTextField();
+		txtEmail.setColumns(10);
+		txtEmail.setText(studentData.getMail());
+		
+		
+		JTextPane txtpnLu = new JTextPane();
+		txtpnLu.setFont(new Font("Tahoma", Font.BOLD, 12));
+		txtpnLu.setBackground(SystemColor.menu);
+		txtpnLu.setText("LU");
+		
+		JTextPane txtpnApellido = new JTextPane();
+		txtpnApellido.setFont(new Font("Tahoma", Font.BOLD, 12));
+		txtpnApellido.setText("Apellido");
+		txtpnApellido.setBackground(SystemColor.menu);
+		
+		JTextPane txtpnNombre = new JTextPane();
+		txtpnNombre.setFont(new Font("Tahoma", Font.BOLD, 12));
+		txtpnNombre.setText("Nombre");
+		txtpnNombre.setBackground(SystemColor.menu);
+		
+		JTextPane txtpnEmail = new JTextPane();
+		txtpnEmail.setFont(new Font("Tahoma", Font.BOLD, 12));
+		txtpnEmail.setText("E-mail");
+		txtpnEmail.setBackground(SystemColor.menu);
+		
+		JTextPane txtpnGithub = new JTextPane();
+		txtpnGithub.setFont(new Font("Tahoma", Font.BOLD, 12));
+		txtpnGithub.setText("GitHub URL");
+		txtpnGithub.setBackground(SystemColor.menu);
+		GroupLayout gl_tabInformation = new GroupLayout(tabInformation);
+		gl_tabInformation.setHorizontalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtpnLu, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnApellido, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnNombre, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnEmail, GroupLayout.PREFERRED_SIZE, 73, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtpnGithub, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE))
+					.addGap(26)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addComponent(txtApellido)
+						.addComponent(txtNombre)
+						.addComponent(txtEmail)
+						.addComponent(txtGithub, GroupLayout.DEFAULT_SIZE, 292, Short.MAX_VALUE)
+						.addComponent(txtLU, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_tabInformation.setVerticalGroup(
+			gl_tabInformation.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_tabInformation.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addComponent(txtLU, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(13)
+							.addComponent(txtApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addComponent(txtpnLu, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(txtpnApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_tabInformation.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(txtEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(11)
+							.addComponent(txtGithub, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_tabInformation.createSequentialGroup()
+							.addComponent(txtpnNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(txtpnEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(txtpnGithub, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		tabInformation.setLayout(gl_tabInformation);
+		
+		JLabel lblImage = new JLabel("New label");
+		lblImage.setIcon(new ImageIcon(SimplePresentationScreen.class.getResource(studentData.getPathPhoto())));
+		
+		txtHora = new JTextPane();
+		txtHora.setBackground(SystemColor.menu);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(tabbedPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addComponent(lblImage, GroupLayout.PREFERRED_SIZE, 149, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(10)
+							.addComponent(txtHora, GroupLayout.PREFERRED_SIZE, 409, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		
+		LocalDateTime time = LocalDateTime.now();
+		DateTimeFormatter formateadorMes = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		DateTimeFormatter formateadorHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+		String textoMes = time.format(formateadorMes);
+		String textoHora = time.format(formateadorHora);
+		String texto = "Esta ventana fue generada "+ textoMes + " a las " + textoHora;
+		txtHora.setText(texto);
+		
+		
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblImage, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE))
+						.addComponent(tabbedPane, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 201, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(txtHora, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,12 +1,24 @@
 package launcher;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import entities.Student;
+import gui.SimplePresentationScreen;
+
 public class Launcher {
 	public static void main(String [] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        	Student alumno = new Student( 136205, "Rojo", "Federico", "federojo10@gmail.com", "https://github.com/FedericoRojo", "/images/Screenshot_1.png");
             public void run() {
-            	
+            	SimplePresentationScreen s = new SimplePresentationScreen(alumno);
+            	s.setVisible(true);
             }
         });
     }
